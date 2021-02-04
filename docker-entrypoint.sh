b4a6a1130f47400428c6f2468b36f6dc5d7b52e6
Grégoire Willmann <5655900+GWillmann@users.noreply.github.com>
2021-02-04 17:41:43 +0000

    Improve entrypoint with find instead of loop

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-  echo "Provide a files/path to check for spelling mistakes. Usage: $0 [path to files/folder]"
-  exit 1
-fi
-
-COMMON_ARGS=(--ignore 2 --encoding=utf-8 --word-list-path="/wordlists/all.txt")
-
 echo '"Word", "Number of occurences"'
 
 set -e
-set -x
+set +x
 
-for f in $1 ; do 
-  aspell --lang=en "${COMMON_ARGS[@]}" list < "$f" | aspell --lang=fr "${COMMON_ARGS[@]}" list | sort | uniq -c | awk '{ printf "\"%s\", %s\n", $2, $1 }'
-done
+SPELLING_MISTAKES=$(find "$PATH_TO_CHECK" -type f -name "$EXT_TO_CHECK" -exec bash -c "aspell --lang=en --ignore=2 --encoding=utf-8 --word-list-path=/wordlists/all.txt list < {}" \;)
+SPELLING_MISTAKES=$(echo "$SPELLING_MISTAKES" | aspell --lang=fr --ignore=2 --encoding=utf-8 --word-list-path=/wordlists/all.txt list)
+
+echo "$SPELLING_MISTAKES" | sort | uniq -c | awk '{ printf "\"%s\", %s\n", $2, $1 }'
